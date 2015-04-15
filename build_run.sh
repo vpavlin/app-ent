265206d8af934a00f13606bb51bacd82ddedd06b
langdon <langdon@fedoraproject.org>
2015-04-15 14:24:37 -0400

    cleaned up the build_run script to give better directions/usage

--- build_run.sh
+++ build_run.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
+#use this to build a docker container to run the application
 if [ -z "$USERNAME" ]; then
     echo "setting USERNAME to " `whoami` 
     USERNAME=`whoami`
 fi
 
-echo docker build --rm -t $USERNAME/atomicapp-run .
-
-
+ln -sf Dockerfile.fedora Dockerfile
 docker build --rm -t $USERNAME/atomicapp-run .
+echo The application is now built as a command line executable docker container. 
+echo
+echo You can run it using:
+echo \"docker run -it --privileged -v /run:/run -v /:/host -v `pwd`:/application-entity $USERNAME/atomicapp-run\" 
+echo or, you can create an alias:
+echo alias nulecule=\"docker run -it --privileged -v /run:/run -v /:/host -v `pwd`:/application-entity $USERNAME/atomicapp-run \"
+echo
+echo If you aren\'t sure what to do next, try \(after creating the alias\):
+echo nulecule --help
+echo
 
-#doesn't really make sense to run it
-#test
-#docker run -it --privileged -v /run:/run -v /:/host -v `pwd`:/application-entity $USERNAME/atomicapp-run /bin/bash
-#run
-#docker run -dt --privileged -v /run:/run -v /:/host -v `pwd`:/application-entity $USERNAME/atomicapp-run
