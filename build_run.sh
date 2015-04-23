ed4ccd8b8e6d22bc47ff27cce3f067b8bc1b899f
langdon <langdon@fedoraproject.org>
2015-04-23 13:13:44 -0400

    ok, this seems to work fairly well, however, there is an issue that the version of docker/kuber may mismatch what is on the host

--- build_run.sh
+++ build_run.sh
@@ -6,14 +6,22 @@ if [ -z "$USERNAME" ]; then
     USERNAME=`whoami`
 fi
 
+DIR=$( dirname "${BASH_SOURCE[0]}" )
+echo $DIR
+
+pushd $DIR
 ln -sf Dockerfile.fedora Dockerfile
 docker build --rm -t $USERNAME/atomicapp-run .
+popd
+
+COMMAND="\"docker run --rm -it --privileged -v /run:/run -v /:/host -v \\\`pwd\\\`:/application-entity $USERNAME/atomicapp-run\" "
+
 echo The application is now built as a command line executable docker container. 
 echo
 echo You can run it using:
-echo \"docker run -it --privileged -v /run:/run -v /:/host -v `pwd`:/application-entity $USERNAME/atomicapp-run\" 
+echo $COMMAND
 echo or, you can create an alias:
-echo alias nulecule=\"docker run -it --privileged -v /run:/run -v /:/host -v `pwd`:/application-entity $USERNAME/atomicapp-run \"
+echo alias nulecule=$COMMAND
 echo
 echo If you aren\'t sure what to do next, try \(after creating the alias\):
 echo nulecule --help
