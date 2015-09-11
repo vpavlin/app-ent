0cd06970122098d029761e03fe5dcae0113396d8
Ratnadeep Debnath <rtnpro@gmail.com>
2015-09-11 19:09:57 +0530

    Added docker-compose provider #265

---
+++ atomicapp/providers/docker_compose.py
@@ -0,0 +1,56 @@
+"""
+ Copyright 2015 Red Hat, Inc.
+
+ This file is part of Atomic App.
+
+ Atomic App is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Atomic App is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with Atomic App. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from atomicapp.plugin import Provider, ProviderFailedException
+import os
+import subprocess
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class DockerComposeProvider(Provider):
+    key = "docker-compose"
+
+    def init(self):
+        pass
+
+    def deploy(self):
+        for artifact in self.artifacts:
+            artifact_path = os.path.join(self.path, artifact)
+
+            cmd = "docker-compose -f {} up -d".format(artifact_path)
+            logger.info("DRY-RUN: {}".format(cmd))
+
+            if self.dryrun:
+                logger.info("DRY-RUN: {}".format(cmd))
+            else:
+                subprocess.check_call(cmd.split(" "))
+
+    def undeploy(self):
+        for artifact in self.artifacts:
+            artifact_path = os.path.join(self.path, artifact)
+
+            cmd = "docker-compose -f {} stop".format(artifact_path)
+
+            if self.dryrun:
+                logger.info("DRY-RUN: {}".format(cmd))
+            else:
+                subprocess.check_call(cmd.split(" "))
