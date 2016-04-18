ba16abd259832db0b4fedda2e1ec5c4139e3a96f
Tomas Kral <tkral@redhat.com>
2016-04-18 17:51:23 +0200

    Improve kubeconfig parsing
    add support for geting certificates and keys from '-data'

--- atomicapp/providers/lib/kubeconfig.py
+++ atomicapp/providers/lib/kubeconfig.py
@@ -1,4 +1,6 @@
 import anymarkup
+import os
+from base64 import b64decode
 
 from atomicapp.plugin import ProviderFailedException
 from atomicapp.constants import (PROVIDER_AUTH_KEY,
@@ -8,6 +10,7 @@ from atomicapp.constants import (PROVIDER_AUTH_KEY,
                                  PROVIDER_TLS_VERIFY_KEY,
                                  PROVIDER_CA_KEY)
 import logging
+from atomicapp.utils import Utils
 logger = logging.getLogger(LOGGER_DEFAULT)
 
 
@@ -68,7 +71,7 @@ class KubeConfig(object):
             dict of parsed values from config
         """
         url = None
-        token = None
+        auth = None
         namespace = None
         tls_verify = True
         ca = None
@@ -103,16 +106,52 @@ class KubeConfig(object):
         logger.debug("user: %s", user)
 
         url = cluster["cluster"]["server"]
-        token = user["user"]["token"]
-        if "namespace" in context["context"]:
-            namespace = context["context"]["namespace"]
-        if "insecure-skip-tls-verify" in cluster["cluster"]:
-            tls_verify = not cluster["cluster"]["insecure-skip-tls-verify"]
-        elif "certificate-authority" in cluster["cluster"]:
-            ca = cluster["cluster"]["certificate-authority"]
+        auth = user["user"].get("token")
+        namespace = context["context"].get("namespace")
+        tls_verify = not cluster["cluster"].get("insecure-skip-tls-verify")
+
+        if tls_verify:
+            ca_data = cluster["cluster"].get("certificate-authority-data")
+            if ca_data:
+                ca = Utils.getTmpFile(b64decode(ca_data))
+            else:
+                if "certificate-authority" in cluster["cluster"]:
+                    # if we are in container translate path to path on host
+                    ca = os.path.join(Utils.getRoot(),
+                                      cluster["cluster"].get("certificate-authority").lstrip('/'))
+
+        if not auth:
+            # If token not specified, check for certificate auth.
+
+            # client-certificate-data and client-key-data options overrides
+            # client-certificate and client-key
+            # https://github.com/kubernetes/kubernetes/blob/v1.2.2/pkg/client/unversioned/clientcmd/api/types.go#L78
+            # `{client-certificate,client-key}-data` keys in Kubernetes config
+            # file are inline base64 encoded certificates, requests library
+            # requires certs in files, this is why we are putting them to tmp
+            # files.
+
+            cert_data = user["user"].get("client-certificate-data")
+            key_data = user["user"].get("client-key-data")
+
+            if cert_data:
+                cert = Utils.getTmpFile(b64decode(cert_data))
+            else:
+                if "client-certificate" in user["user"]:
+                    cert = os.path.join(Utils.getRoot(),
+                                        user["user"].get("client-certificate").lstrip('/'))
+
+            if key_data:
+                key = Utils.getTmpFile(b64decode(key_data))
+            else:
+                if "client-key" in user["user"]:
+                    key = os.path.join(Utils.getRoot(),
+                                       user["user"].get("client-key").lstrip('/'))
+
+            auth = "{}:{}".format(cert, key)
 
         return {PROVIDER_API_KEY: url,
-                PROVIDER_AUTH_KEY: token,
+                PROVIDER_AUTH_KEY: auth,
                 NAMESPACE_KEY: namespace,
                 PROVIDER_TLS_VERIFY_KEY: tls_verify,
                 PROVIDER_CA_KEY: ca}
